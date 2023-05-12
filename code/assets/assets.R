197c4957fd363da01aa6f347d74831277b028ac1
apposada <ap.posada1@gmail.com>
2023-05-12 18:07:46 +0100

    added assets directory and functional category lookup table

---
+++ code/assets/assets.R
@@ -0,0 +1,33 @@
+funcat_lookup <- data.frame(
+ cog = 
+   c(
+     "J", "A", "K", "L", "B", "D", "Y", "V", "T", "M", "N",
+     "Z", "W", "U", "O", "X", "C", "G", "E", "F", "H", "I",
+     "P", "Q", "R", "S"
+   ),
+ functional_category = 
+   c(
+     "Translation, Ribosomal Structure and Biogenesis",
+     "RNA Processing and Modification","Transcription",
+     "Replication, Recombination and Repair",
+     "Chromatin Structure and Dynamics",
+     "Cell Cycle Control, Cell Division, Chromosome Partitioning",
+     "Nuclear Structure","Defense Mechanisms",
+     "Signal Transduction Mechanisms",
+     "Cell Wall/Membrane/Envelope Biogenesis","Cell Motility",
+     "Cytoskeleton","Extracellular Structures",
+     "Intracellular Trafficking, Secretion, and Vesicular Transport",
+     "PostTranslational Modification, Protein Turnover, Chaperones",
+     "Mobilome: Prophages, Transposons",
+     "Energy Production and Conversion",
+     "Carbohydrate Transport and Metabolism",
+     "Amino Acid Transport and Metabolism",
+     "Nucleotide Transport and Metabolism",
+     "Coenzyme Transport and Metabolism",
+     "Lipid Transport and Metabolism",
+     "Inorganic Ion Transport and Metabolism",
+     "Secondary Metabolyte Biosynthesis, Transport and Catabolism",
+     "General Function Prediction Only","Function Unknown"
+   )
+)
+
---
+++ code/assets/functional_categories.R
@@ -0,0 +1,26 @@
+J	Translation, Ribosomal Structure and Biogenesis
+A	RNA Processing and Modification
+K	Transcription
+L	Replication, Recombination and Repair
+B	Chromatin Structure and Dynamics
+D	Cell Cycle Control, Cell Division, Chromosome Partitioning
+Y	Nuclear Structure
+V	Defense Mechanisms
+T	Signal Transduction Mechanisms
+M	Cell Wall/Membrane/Envelope Biogenesis
+N	Cell Motility
+Z	Cytoskeleton
+W	Extracellular Structures
+U	Intracellular Trafficking, Secretion, and Vesicular Transport
+O	PostTranslational Modification, Protein Turnover, Chaperones
+X	Mobilome: Prophages, Transposons
+C	Energy Production and Conversion
+G	Carbohydrate Transport and Metabolism
+E	Amino Acid Transport and Metabolism
+F	Nucleotide Transport and Metabolism
+H	Coenzyme Transport and Metabolism
+I	Lipid Transport and Metabolism
+P	Inorganic Ion Transport and Metabolism
+Q	Secondary Metabolyte Biosynthesis, Transport and Catabolism
+R	General Function Prediction Only
+S	Function Unknown
